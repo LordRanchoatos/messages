f03d135b765307fc1ee68335f43c72207a9c75d6
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-10-21 15:56:40 +0100

    get all messages

--- src/messages/massages.repository.ts
+++ src/messages/massages.repository.ts
@@ -9,10 +9,13 @@ export class MessagesRepository {
     }
 
     async findAll(){
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
 
+        return messages
     }
 
     async create(message: string) {
-        
+
     }
 }
