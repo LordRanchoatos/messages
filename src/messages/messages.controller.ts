526b70cc94fb2ae91db3a6333a20e9072925acf3
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-10-21 01:07:20 +0100

    accessing request data with decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,12 +8,14 @@ export class MessagesController {
     }
 
     @Post()
-    createMessages(){
+    createMessages(@Body() body: any){
+        console.log(body)
         return 'create a message'
     }
 
     @Get('/:id')
-    getMessage(){
+    getMessage(@Param('id') id: string){
+        console.log(id)
         return 'a particular messsage'
     }
 }
